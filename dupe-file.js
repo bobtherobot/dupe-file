a208734acb7392e28f62601e6bdd9409a8bec6a3
Michael Gieson <mgieson@plaino.com>
2019-10-20 02:32:48 -0400

    first

---
+++ dupe-file.js
@@ -0,0 +1,49 @@
+/*
+--------------
+Dupe File
+--------------
+By Mike Gieson (www.gieson.com)
+
+Special thanks to:
+Dennis Malinochkin (mrmlnc.com)
+
+*/
+
+
+"use strict";
+
+var vscode = require("vscode");
+var myfs = require("myfs");
+
+function dupe(uri, settings) {
+
+	var newPath = myfs.dupe(uri.path);
+	
+    setTimeout(function(){
+
+        var openPath = vscode.Uri.file(newPath);
+            vscode.workspace.openTextDocument(openPath).then(doc => {
+            vscode.window.showTextDocument(doc);
+        });
+
+    }, 500); // gaylord timer, cheap and easy.
+    
+}
+
+function activate(context) {
+    var command = vscode.commands.registerCommand('dupe.execute', function (uri) {
+
+        var settings = vscode.workspace.getConfiguration().get('dupe');
+        if (!uri || !uri.fsPath) {
+            var editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return;
+            }
+            return dupe(editor.document.uri, settings);
+        }
+        return dupe(uri, settings);
+    });
+    context.subscriptions.push(command);
+}
+
+exports.activate = activate;
